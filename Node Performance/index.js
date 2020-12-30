dffd16160b581af4b419de29eb0889e533d4a8d6
dineshdsv3 <dineshdsv3@gmail.com>
2020-12-30 21:17:25 +0530

    Clustering mode - forkingChildren - 1

--- Node Performance/index.js
+++ Node Performance/index.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-// Function to keep CPU super busy for the duration provided
-// Blocking the event loop here
-function doWork(duration) {
-	let start = Date.now();
-	while (Date.now() - start < duration) {}
+// Is the file being executed in 'Master' mode?
+if (cluster.isMaster) {
+	// Cause index.js to be executed again but in child mode
+	cluster.fork();
+} else {
+	// This is the child mode, It is going to act like a server nothing else.
+	const express = require('express');
+	const app = express();
+
+	// Function to keep CPU super busy for the duration provided
+	// Blocking the event loop here
+	function doWork(duration) {
+		let start = Date.now();
+		while (Date.now() - start < duration) {}
+	}
+
+	app.get('/', (req, res) => {
+		doWork(5000);
+		res.send('Hi There');
+	});
+
+	app.listen(3100, () => {
+		console.log(`Port running on 3100`);
+	});
 }
-
-app.get('/', (req, res) => {
-	doWork(5000);
-	res.send('Hi There');
-});
-
-app.listen(3100, () => {
-	console.log(`Port running on 3100`);
-});
