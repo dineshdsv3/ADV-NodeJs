98483f09c5b17720b862fda0b7dcec2f067593bd
dineshdsv3 <dineshdsv3@gmail.com>
2021-01-07 16:45:49 +0530

    Creating a unique KEY for Redis

--- MainProject/services/cache.js
+++ MainProject/services/cache.js
@@ -5,6 +5,16 @@ const exec = mongoose.Query.prototype.exec;
 // Here we need to ignore arrow function because it doesn't have own "this"
 mongoose.Query.prototype.exec = function () {
 	console.log('This function will run before the query execution');
+	// gives us the query which we ran in route
+	//  console.log(this.getQuery());
+	// gives us the collection name which we ran
+	//  console.log(this.mongooseCollection.name);
+
+	// Creating a key for redis cache
+	const key = Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name });
+
+	console.log(key);
+
 	// Below line is to run the query execution as it is from mongoose
 	return exec.apply(this, arguments);
 };
